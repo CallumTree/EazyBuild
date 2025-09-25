f62c2e05c512568acd879a423930c7ffad69b93e
CallumTree <callumtree@googlemail.com>
2025-09-25 16:04:09 +0000

    Restored to 'a745a9a97a4b956a15779029985944c8beb67541'
    Replit-Restored-To: a745a9a97a4b956a15779029985944c8beb67541

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,11 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { ViabilityProvider } from './store/viability'
 import './index.css'
 import 'leaflet/dist/leaflet.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<ViabilityProvider>
-			<App />
-		</ViabilityProvider>
+		<App />
 	</React.StrictMode>
 )
